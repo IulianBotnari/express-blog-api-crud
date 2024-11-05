cff1ce5e70fa1aec9ac73f6df8d73d2335f42477
IulianBotnari <iulianbotnari93@gmail.com>
2024-11-05 17:52:18 +0100

    FIx img bug

--- public/js/main.js
+++ public/js/main.js
@@ -1,32 +1,24 @@
 axios.get("http://127.0.0.1:3000/posts")
     .then(response => {
         const posts = response.data
-        
+
+        let counter = 0
         posts.forEach(element => {
             const bodyEL = document.querySelector("body")
             const singlePost = element
             let { title, slug, content, image, tags } = singlePost
-           
+            counter += 1
 
             let markup = `
-                <h1>Post</h1>
+                <h1>Post ${counter}</h1>
                 <p>${title}</p>
                 <p>${slug}</p>
                 <p>${content}</p>
-                <img scr="${image.src}" alt="Immagine">
-                <p>${tags}</p>
+                <img src="${image}" alt="Immagine" width="500">
+                <p>${tags.join(", ")}</p>
             `
             bodyEL.insertAdjacentHTML("beforeend", markup)
         });
-
-
-
-
-
-
-
-
-
 
 
     }
@@ -34,3 +26,11 @@ axios.get("http://127.0.0.1:3000/posts")
     .catch(error => {
         console.error(`Error: ${error}`)
     })
+
+
+
+
+
+
+
+
