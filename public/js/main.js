96d66a032fff9e41050db62d64700141e050124d
IulianBotnari <iulianbotnari93@gmail.com>
2024-11-05 17:20:58 +0100

    Add Html

--- public/js/main.js
+++ public/js/main.js
@@ -1,11 +1,36 @@
 axios.get("http://127.0.0.1:3000/posts")
-.then(response => {
-    const posts = response.data
-    const bodyEL = document.querySelector("body")
+    .then(response => {
+        const posts = response.data
+        
+        posts.forEach(element => {
+            const bodyEL = document.querySelector("body")
+            const singlePost = element
+            let { title, slug, content, image, tags } = singlePost
+           
 
-    bodyEL.insertAdjacentHTML("beforeend", posts)
-}
-)
-.catch(error =>{
-    console.error(`Error: ${error}`)
-})
+            let markup = `
+                <h1>Post</h1>
+                <p>${title}</p>
+                <p>${slug}</p>
+                <p>${content}</p>
+                <img scr="${image.src}" alt="Immagine">
+                <p>${tags}</p>
+            `
+            bodyEL.insertAdjacentHTML("beforeend", markup)
+        });
+
+
+
+
+
+
+
+
+
+
+
+    }
+    )
+    .catch(error => {
+        console.error(`Error: ${error}`)
+    })
