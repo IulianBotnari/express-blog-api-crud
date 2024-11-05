0693a148afc433e2437e25cf39856cebd9208784
IulianBotnari <iulianbotnari93@gmail.com>
2024-11-05 15:35:36 +0100

    Delete previos file

--- router/routes.js
+++
@@ -1,8 +0,0 @@
-const express = require(`express`)
-const router = express.Router()
-
-const carController = require(`../controllers/controllers.js`)
-router.get(`/cars`, carController.carModelApi)
-router.post(`/cars`, carController.addCarModel)
-
-module.exports = router
